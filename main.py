c4da06b3d2dd1ee950f2fbed9eaabd479f0ec139
JeremiahUy <jery_3000@hotmail.com>
2020-10-19 13:47:19 +0200

    COMMENTED: code for debugging

--- main.py
+++ main.py
@@ -53,11 +53,11 @@ def application_startup():
         logger.warning("No KUBERNETES_SERVICE_HOST set in env.")
 
     # Loading kubernetes config
-    init_kube_client()
-    v1 = client.CustomObjectsApi()
-    v1.list_cluster_custom_object(group="nais.io",
-                                  version="v1alpha1",
-                                  plural="applications")
+    # init_kube_client()
+    # v1 = client.CustomObjectsApi()
+    # v1.list_cluster_custom_object(group="nais.io",
+    #                               version="v1alpha1",
+    #                               plural="applications")
 
     threading.Thread(target=watch_nais_task, daemon=True).start()
 
