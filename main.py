19aee1748f590bfca962290388d13c9eacf3d342
JeremiahUy <jery_3000@hotmail.com>
2020-10-19 15:21:07 +0200

    COMMENTED: code for testing

--- main.py
+++ main.py
@@ -54,10 +54,10 @@ def application_startup():
 
     # Loading kubernetes config
     init_kube_client()
-    v1 = client.CustomObjectsApi()
-    v1.list_cluster_custom_object(group="nais.io",
-                                  version="v1alpha1",
-                                  plural="applications")
+    # v1 = client.CustomObjectsApi()
+    # v1.list_cluster_custom_object(group="nais.io",
+    #                              version="v1alpha1",
+    #                              plural="applications")
 
     threading.Thread(target=watch_nais_task, daemon=True).start()
 
