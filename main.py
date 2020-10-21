ace833d19a6e6ae420fb25c6fa9fcd82566e5c1b
JeremiahUy <jery_3000@hotmail.com>
2020-10-21 11:51:53 +0200

    UPDATE: to only post to ingress-retriever on the same cluster(dev to dev, prod to prod)

--- main.py
+++ main.py
@@ -25,10 +25,8 @@ def watch_nais_callback(e):
     logger.info("Event: %s %s (%s)" % (e['type'], e['object']['metadata']['name'], e['object']['metadata']['uid']))
     e.pop("type")
     e["cluster"] = os.environ["NAIS_CLUSTER_NAME"]
-    logger.info("Posting " + e['object']['metadata']['name'] + " to ingress-retriever prod")
-    request_put('https://ingress-retriever.prod-gcp.nais.io/event', e)
-    logger.info("Posting " + e['object']['metadata']['name'] + " to ingress-retriever dev")
-    request_put('https://ingress-retriever.dev-gcp.nais.io/event', e)
+    logger.info("Posting " + e['object']['metadata']['name'] + " to ingress-retriever " + os.environ["NAIS_CLUSTER_NAME"])
+    request_put(os.environ["RETRIEVER_URL"], e)
 
 
 def init_kube_client():
